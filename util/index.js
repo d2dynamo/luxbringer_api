125ed01408cfce6879c10c9b79bf4cb0cac15a17
d2dynamo <d2dynamo@gmail.com>
2021-04-19 13:29:52 +0300

    added cronDatadragonUpdater to import and export from util/index

--- util/index.js
+++ util/index.js
@@ -1,10 +1,12 @@
 const {logger, userLogger} = require("./Loggers");
 const downloadFile = require("./Downloader");
 const DatadragonUpdater = require("./DatadragonUpdater");
+const { cronDatadragonUpdater } = require("./CronJobs");
 
 module.exports = {
     logger,
     userLogger,
     downloadFile,
-    DatadragonUpdater
+    DatadragonUpdater,
+    cronDatadragonUpdater
 }
